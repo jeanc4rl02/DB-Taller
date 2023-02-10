ebed876c367e84cb04aeb91be7f77ccd93d3898d
jeanc4rl02 <jean.carrillo@udea.edu.co>
2023-02-10 17:22:20 -0500

    Validación adicional Reception

--- Reception/reception.js
+++ Reception/reception.js
@@ -33,9 +33,12 @@ new Vue({
   }, 
   methods: {
     findClient(){
-        let person = this.clients.filter(c => c.id == this.clientEntered);
+      if(this.clients){
+        const person = this.clients.filter(c => c.id == this.clientEntered);
         this.nameEntered = person[0].name;
-      
+      } else {
+        this.error = true;
+      }
       if(this.vehicles != null){
         const vehicle = this.vehicles.filter(v => v.id == this.clientEntered);
         this.carsFound = vehicle;
@@ -71,11 +74,12 @@ new Vue({
         state: "Sin revisar",
         detalle: []
       })
+      localStorage.setItem("carsEntries", JSON.stringify(this.newEntry));
+      window.location.reload();
       } else {
         this.errorEntry = true;
       }
-      localStorage.setItem("carsEntries", JSON.stringify(this.newEntry));
-      window.location.reload();
+      
     },
     generateNewVehicle(){
       if(this.modalId != null && this.modalPlate != null && this.modalBrand != null && this.modalModel != null){
